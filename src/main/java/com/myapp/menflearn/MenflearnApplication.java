ae06f4a471ce4b4fa5302e6f07174ba2bc7d301b
JoHyunwoo <jhw3874@gmail.com>
2025-06-16 21:46:56 +0900

    first commit

---
+++ src/main/java/com/myapp/menflearn/MenflearnApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.menflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenflearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenflearnApplication.class, args);
+	}
+
+}
